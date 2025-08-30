27d2cc3aee1f0bf0bc5b86ec714d57b3eba6aeb5
baban-afnan <shafiumuhd114@gmail.com>
2025-08-30 13:36:06 +0100

    Update: moving project to FZ001-2025 repo

--- app/Http/Controllers/NINverificationController.php
+++ app/Http/Controllers/NINverificationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\noncestrHelper;
+use App\Helpers\signatureHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -118,23 +120,105 @@ class NINverificationController extends Controller
                 'Found in DB'
             );
         }
+          try {
 
-        // 6. Call external API if not in DB
-        try {
-            $apiResponse = Http::timeout(30)->withHeaders([
-                'accept' => 'application/json',
-                'content-type' => 'application/json',
-                'x-api-key' => env('PREMBLY_API_KEY'),
-                'app-id' => env('PREMBLY_APP_ID'),
-            ])->post('https://api.prembly.com/identitypass/verification/vnin', [
-                'number_nin' => $validated['number_nin']
-            ]);
-        } catch (\Exception $e) {
-            return back()->with([
-                'status' => 'error',
-                'message' => 'API connection failed: ' . $e->getMessage()
-            ]);
-        }
+                $requestTime = (int) (microtime(true) * 1000);
+
+            $noncestr = noncestrHelper::generateNonceStr();
+
+            $data = [
+
+                'version' => env('VERSION'),
+                'nonceStr' => $noncestr,
+                'requestTime' => $requestTime,
+                'nin' => $request->nin,
+            ];
+
+            $signature = signatureHelper::generate_signature($data, config('keys.private2'));
+
+            $url = env('Domain') . '/api/validator-service/open/nin/inquire';
+            $token = env('BEARER');
+
+                $headers = [
+                    'Accept: application/json, text/plain, */*',
+                    'Content-Type: application/json',
+                    "Authorization: Bearer $token",
+                ];
+
+                // Initialize cURL
+                $ch = curl_init();
+
+                // Set cURL options
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+                // Execute request
+                $response = curl_exec($ch);
+
+                // Check for cURL errors
+                if (curl_errno($ch)) {
+                    throw new \Exception('cURL Error: '.curl_error($ch));
+                }
+
+                // Close cURL session
+                curl_close($ch);
+
+                $response = json_decode($response, true);
+
+                if (isset($response['respCode']) && $response['respCode'] == '000') {
+
+                    $data = $response['data'];
+
+                    $this->processResponseDataForNIN($data);
+
+                    $balance = $wallet->balance - $ServiceFee;
+
+                    Wallet::where('user_id', $loginUserId)
+                        ->update(['balance' => $balance]);
+
+                    $serviceDesc = 'Wallet debitted with a service fee of ₦'.number_format($ServiceFee, 2);
+
+                    $this->transactionService->createTransaction($loginUserId, $ServiceFee, 'NIN Verification', $serviceDesc, 'Wallet', 'Approved');
+
+                    return json_encode(['status' => 'success', 'data' => $data]);
+                } elseif (in_array($response['respCode'], ['100', '101', '102', '103'])) {
+
+                    return response()->json([
+                        'status' => 'Not Found',
+                        'errors' => ['No record found'],
+                    ], 422);
+                } else {
+                    return response()->json([
+                        'status' => 'Verification Failed',
+                        'errors' => ['Verification Failed: No need to worry, your wallet remains secure and intact. Please try again or contact support for assistance.'],
+                    ], 422);
+                }
+            } catch (\Exception $e) {
+                return response()->json([
+                    'status' => 'Request failed',
+                    'errors' => ['An error occurred while making the API request'],
+                ], 422);
+            }
+
+        // // 6. Call external API if not in DB
+        // try {
+        //     $apiResponse = Http::timeout(30)->withHeaders([
+        //         'accept' => 'application/json',
+        //         'content-type' => 'application/json',
+        //         'x-api-key' => env('PREMBLY_API_KEY'),
+        //         'app-id' => env('PREMBLY_APP_ID'),
+        //     ])->post('https://api.prembly.com/identitypass/verification/vnin', [
+        //         'number_nin' => $validated['number_nin']
+        //     ]);
+        // } catch (\Exception $e) {
+        //     return back()->with([
+        //         'status' => 'error',
+        //         'message' => 'API connection failed: ' . $e->getMessage()
+        //     ]);
+        // }
 
         $responseData = $apiResponse->json();
 
